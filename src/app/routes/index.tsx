5809cd6f77d600ee49fcf01e56ff5ab0910efebc
Smirnov Vitalii <temmets20@gmail.com>
2024-06-23 19:31:24 +0700

    fix routing

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,7 +1,9 @@
 import { Navigate, useRoutes } from 'react-router-dom'
 import { lazy, Suspense } from 'react'
-import { ProtectedRoutes } from './ProtectedRoutes'
+import { ProtectedAdminRoutes, ProtectedRoutes } from './ProtectedRoutes'
 import { RouteType } from './RouteType.ts'
+import { Spin } from 'antd'
+import { RolesEnum } from '../../shared/types/user/RolesEnum.ts'
 
 const Auth = lazy(() => import('../../pages/User/Auth'))
 const AdminAuth = lazy(() => import('../../pages/Admin/Auth'))
@@ -18,18 +20,35 @@ const createRoutes = (isAuth: boolean) => {
       element: <Auth />,
     },
     {
+      path: '*',
+      element: <Navigate to={RouteType.STUDENT_INTERNSHIP_PROGRESS} />,
+    },
+  ]
+}
+
+const createAdminRoutes = (isAuth: boolean) => {
+  return [
+    {
+      element: isAuth ? <MainLayout /> : <Navigate to={RouteType.ADMIN_LOGIN} />,
+      children: ProtectedAdminRoutes,
+    },
+    {
       path: RouteType.ADMIN_LOGIN,
       element: <AdminAuth />,
     },
     {
       path: '*',
-      element: <Navigate to={'/'} />,
+      element: <Navigate to={RouteType.ADMIN_LISTS} />,
     },
   ]
 }
 
-export const Router = () => {
-  const isAuth = localStorage.getItem('userToken')
-  const customRouter = useRoutes(createRoutes(!!isAuth))
-  return <Suspense fallback={<span>{'loading'}</span>}>{customRouter}</Suspense>
+interface RouterProps {
+  isAuth: boolean
+  role: RolesEnum
+}
+
+export const Router = ({ role, isAuth }: RouterProps) => {
+  const customRouter = useRoutes(role.includes(RolesEnum.ADMIN) ? createAdminRoutes(isAuth) : createRoutes(isAuth))
+  return <Suspense fallback={<Spin />}>{customRouter}</Suspense>
 }
--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -2,23 +2,18 @@ import { Outlet } from 'react-router-dom'
 import { Navbar } from '../../entities/ui/Navbar'
 import { useGetProfileQuery } from '../../shared/api/Auth/AuthRequest.ts'
 import { Spin } from 'antd'
+import { Suspense } from 'react'
 
 const MainLayout = () => {
-  const { data, isLoading } = useGetProfileQuery({})
+  const { data } = useGetProfileQuery({})
 
   return (
-    <div style={{ width: '100%', height: '100%' }}>
-      {!isLoading ? (
-        <>
-          <Navbar profile={data} />
-          <div style={{ padding: '1rem' }}>
-            <Outlet />
-          </div>
-        </>
-      ) : (
-        <Spin />
-      )}
-    </div>
+    <Suspense fallback={<Spin />}>
+      <Navbar profile={data} />
+      <div style={{ padding: '1rem' }}>
+        <Outlet />
+      </div>
+    </Suspense>
   )
 }
 
